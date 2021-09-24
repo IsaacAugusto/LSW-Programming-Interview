610cf588f5669296b50e73affd2367f25b8b96b1
Isaac Augusto <isaac.augusto@hotmail.com>
2021-09-24 16:49:42 -0300

    Added basic player movement

---
+++ Assets/Scripts/Player/PlayerBehaviour.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    private Rigidbody2D _rb;
+    private Animator _anim;
+    private bool _canMove = true;
+
+    private Vector2 _moveAxis;
+
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetMoveAxis();
+        FlipCharacter();
+        ControllAnimatorParams();
+    }
+
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    /// <summary>
+    /// Read Input Axis and set on _moveAxis.
+    /// </summary>
+    private void GetMoveAxis()
+    {
+        _moveAxis.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    /// <summary>
+    /// Apply movement on the player rigidbody using the moveAxis and speed
+    /// </summary>
+    private void Movement()
+    {
+        if (_canMove)
+        {
+            _rb.velocity = _moveAxis.normalized * _speed;
+        }
+        else
+        {
+            _rb.velocity = Vector2.zero;
+        }
+    }
+
+    private void FlipCharacter()
+    {
+        if (transform.rotation.eulerAngles.y == 0 && _moveAxis.x < 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        else if (transform.rotation.eulerAngles.y != 0 && _moveAxis.x > 0)
+        {
+            transform.rotation = Quaternion.identity;
+        }
+    }
+
+    private void ControllAnimatorParams()
+    {
+        _anim.SetFloat("Speed", _rb.velocity.magnitude);
+    }
+}
