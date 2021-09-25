119656d14df5a0776f98aadaa62e0a3a5a11e497
Isaac Augusto <isaac.augusto@hotmail.com>
2021-09-25 15:31:20 -0300

    Added LeanTween package for UI simple animations and updated ItemShop.

--- Assets/Scripts/Shop/ShopItem.cs
+++ Assets/Scripts/Shop/ShopItem.cs
@@ -1,30 +1,51 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ShopItem : MonoBehaviour
+public class ShopItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    public enum ShopItemStatus { CanBuy, CannotBuy, AlreadyOwned }
+
+    public ShopItemStatus ItemStatus;
     [SerializeField] private Image _image;
     [SerializeField] private Text _priceText;
     private int _price;
 
-    public void SetItemData(ClothesSO clothe, int playerCoins)
+    public void SetItemData(ClothesSO clothe, int playerCoins, InventorySO playerInventory)
     {
         _image.sprite = clothe.Sprite;
         _price = clothe.Price;
         _priceText.text = _price.ToString();
 
-        CheckCanBuy(playerCoins);
+        CheckItemStatus(playerCoins, clothe, playerInventory);
     }
 
-    private void CheckCanBuy(int playerCoins)
+    private void CheckItemStatus(int playerCoins, ClothesSO item, InventorySO playerInventory)
     {
-        if (playerCoins < _price)
+        if (playerInventory.OwnedClothes.Contains(item))
+        {
+            _priceText.gameObject.SetActive(false);
+            ItemStatus = ShopItemStatus.AlreadyOwned;
+        }
+        else if (playerCoins < _price)
         {
             _priceText.color = Color.red;
+            ItemStatus = ShopItemStatus.CannotBuy;
         }
         else
         {
             _priceText.color = Color.white;
+            ItemStatus = ShopItemStatus.CanBuy;
         }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
     }
 }
--- Assets/Scripts/Shop/ShopManager.cs
+++ Assets/Scripts/Shop/ShopManager.cs
@@ -12,6 +12,14 @@ public class ShopManager : MonoBehaviour
     [SerializeField] private RectTransform _itemsHolder;
     [SerializeField] private GameObject _itemPrefab;
 
+    [Space]
+    [Header("Buttons")]
+    [SerializeField] private Button _buyButton;
+    [SerializeField] private Button _sellButton;
+    [SerializeField] private Button _equipButton;
+
+    private ShopItem _selectedItem;
+
     private void Start()
     {
         _playerCoinsText.text = PlayerInventory.Coins.ToString();
@@ -23,8 +31,13 @@ public class ShopManager : MonoBehaviour
         ClearView();
         foreach(ClothesSO clothe in ShopGoods.Items)
         {
-            Instantiate(_itemPrefab, _itemsHolder).GetComponent<ShopItem>().SetItemData(clothe, PlayerInventory.Coins);
+            Instantiate(_itemPrefab, _itemsHolder).GetComponent<ShopItem>().SetItemData(clothe, PlayerInventory.Coins, PlayerInventory);
         }
+    }
+
+    public void CloseShop()
+    {
+        Destroy(this.gameObject);
     }
 
     private void ClearView()
@@ -32,6 +45,13 @@ public class ShopManager : MonoBehaviour
         foreach(Transform child in _itemsHolder)
         {
             Destroy(child.gameObject);
+        }
+    }
+
+    private void UpdateButtons()
+    {
+        if (_selectedItem != null)
+        {
         }
     }
 }
