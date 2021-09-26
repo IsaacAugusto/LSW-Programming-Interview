d40eb47d2d902bf80e1bac2e7554aa6df1895e64
Isaac Augusto <isaac.augusto@hotmail.com>
2021-09-26 18:01:59 -0300

    Added sounds on pause menu

--- Assets/Scripts/Pause/PauseCanvas.cs
+++ Assets/Scripts/Pause/PauseCanvas.cs
@@ -6,16 +6,19 @@ public class PauseCanvas : MonoBehaviour
 {
     private void Start()
     {
+        SoundManager.Instance.PlaySound(SoundManager.Instance.Sounds.OpenMenuSound);
         LeanTween.scaleY(gameObject, 1, .2f).setOnComplete( ()=> PlayerInteractions.SetPlayerInteracting(true) );
     }
 
     public void Quit()
     {
+        SoundManager.Instance.PlaySound(SoundManager.Instance.Sounds.ClickSound);
         Application.Quit();
     }
 
     public void Close()
     {
+        SoundManager.Instance.PlaySound(SoundManager.Instance.Sounds.ClickSound);
         PlayerInteractions.SetPlayerInteracting(false);
         LeanTween.scaleY(gameObject, 0, .2f).setOnComplete(() => Destroy(transform.parent.gameObject));
     }
